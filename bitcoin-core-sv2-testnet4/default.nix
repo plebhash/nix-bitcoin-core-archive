96dbbe95adc2011c1c31e0daa8784e256b725793
plebhash <plebhash@proton.me>
2024-05-10 17:12:31 -0300

    add sv2-testnet4

---
+++ bitcoin-core-sv2-testnet4/default.nix
@@ -0,0 +1,61 @@
+{ pkgs ? import <nixpkgs> { system = builtins.currentSystem; }
+, lib ? pkgs.lib
+, stdenv ? pkgs.stdenv
+, fetchFromGitHub ? pkgs.fetchFromGitHub
+, autoreconfHook ? pkgs.autoreconfHook
+, pkg-config ? pkgs.pkg-config
+, util-linux ? pkgs.util-linux
+, hexdump ? pkgs.hexdump
+, autoSignDarwinBinariesHook ? pkgs.autoSignDarwinBinariesHook
+, boost ? pkgs.boost
+, libevent ? pkgs.libevent
+, miniupnpc ? pkgs.miniupnpc
+, zeromq ? pkgs.zeromq
+, zlib ? pkgs.zlib
+, db48 ? pkgs.db48
+, sqlite ? pkgs.sqlite
+, qrencode ? pkgs.qrencode
+, python3 ? pkgs.python3
+, nixosTests ? pkgs.nixosTests
+}:
+
+with lib;
+let
+  version = "v25.99.0";
+  majorVersion = versions.major version;
+in
+stdenv.mkDerivation rec {
+  pname = "bitcoind-sv2-testnet4";
+  inherit version;
+
+  src = fetchFromGitHub {
+    owner = "plebhash";
+    repo = "bitcoin";
+    rev = "5c8442df982aa045c7aebb66a841e6b4b01c9c72";
+    hash = "sha256-ZNt9srJGf65Eh5yrI3kL4ldotfWKbKASxcWGIwZXvPE=";
+  };
+
+  nativeBuildInputs =
+    [ autoreconfHook pkg-config ]
+    ++ optionals stdenv.isLinux [ util-linux ]
+    ++ optionals stdenv.isDarwin [ hexdump ]
+    ++ optionals (stdenv.isDarwin && stdenv.isAarch64) [ autoSignDarwinBinariesHook ];
+
+  buildInputs = [ boost libevent miniupnpc zeromq zlib ];
+
+  configureFlags = [
+    "--with-boost-libdir=${boost.out}/lib"
+    "--disable-bench"
+    "--disable-wallet"
+    "--disable-tests"
+    "--disable-gui-tests"
+  ];
+
+  checkInputs = [ python3 ];
+
+  doCheck = false;
+
+  checkFlags = [ "LC_ALL=en_US.UTF-8" ];
+
+  enableParallelBuilding = true;
+}
